6da9d028c3f1b749fcd89795b1ebefe618c3dca9
TabarekAyad <tabarek.ayad@gmail.com>
2026-04-17 21:51:46 -0400

    refactor: pythonic loop implemented

--- src/recommender.py
+++ src/recommender.py
@@ -128,11 +128,9 @@ def recommend_songs(user_prefs: Dict, songs: List[Dict], k: int = 5) -> List[Tup
     Functional implementation of the recommendation logic.
     Required by src/main.py
     """
-    scored = []
-    for song in songs:
-        score, reasons = score_song(user_prefs, song)
-        scored.append((song, score, reasons))
+    scored = [
+        (song, *score_song(user_prefs, song))
+        for song in songs
+    ]
 
-    scored.sort(key=lambda x: (x[1], x[0]["valence"]), reverse=True)
-
-    return [(song, score, reasons) for song, score, reasons in scored[:k]]
+    return sorted(scored, key=lambda x: (x[1], x[0]["valence"]), reverse=True)[:k]
